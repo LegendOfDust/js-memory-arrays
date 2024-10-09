186a15a23534c0fd46d9a783cb937bcbae2c9b12
Ashlyn Carter <eannacarter@gmail.com>
2024-10-09 10:26:48 -0400

    Completed bonus (no casting spells with 0 manna)

--- js/index.js
+++ js/index.js
@@ -8,16 +8,22 @@ const castSpell = () => {
     const randomSpell = spellBook[randomIndex];
     console.log('randomSpell: ', randomSpell);
 
-    //show the user the spell on the screen
-    const spellElement = document.getElementById('spell');
-    const message = `You cast the ${randomSpell} spell!`;
-    spellElement.textContent = message;
+    if(mannaPoints <= 0){
+        const spellElement = document.getElementById('spell');
+        spellElement.textContent = "You are out of manna";
+    }
+    else{
+        //show the user the spell on the screen
+        const spellElement = document.getElementById('spell');
+        const message = `You cast the ${randomSpell} spell!`;
+        spellElement.textContent = message;
 
-    //subract 5 mana points from the total
-    mannaPoints -= 5;
-    console.log(mannaPoints);
-    
-    //show the user the new mana points on the screen
-    const mannaElement = document.getElementById('manna');
-    mannaElement.textContent = mannaPoints;
+        //subract 5 mana points from the total
+        mannaPoints -= 5;
+        console.log(mannaPoints);
+        
+        //show the user the new mana points on the screen
+        const mannaElement = document.getElementById('manna');
+        mannaElement.textContent = mannaPoints;
+    }
 }
